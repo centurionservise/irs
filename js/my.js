5d0980f5f66985b3543ac62a34f4e787d05834db
serg_KS <centurionservise@rambler.ru>
2018-09-13 23:58:42 +0300

    home first commit

---
+++ js/my.js
@@ -0,0 +1,11 @@
+'use strict';
+$(document).ready(function () {
+//    alert("Hello");
+//    var bt_open = document.getElementById("BtOpen");
+    
+//    bt_close.onclick = function () {
+//        document.getElementById("fr1").style.width = '10%';
+//    }
+
+
+});
